9bca120d04df23857c5cb2dd0f815c58ad2452fd
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-11-14 15:14:31 -0500

    Added dark mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { Button } from './components/ui/button';
+import { ThemeProvider } from '@/components/theme-provider';
+import { ModeToggle } from '@/components/mode-toggle';
+import { Button } from '@/components/ui/button';
 
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-blue-700 font-bold underline">Hello world!</h1>
-      <div>
-        <Button>Click me</Button>
-      </div>
+      <ThemeProvider
+        defaultTheme="system"
+        storageKey="vite-ui-theme"
+      >
+        <h1 className="text-4xl text-blue-700 font-bold underline">Hello world!</h1>
+        <div>
+          <Button>Click me</Button>
+          <ModeToggle />
+        </div>
+      </ThemeProvider>
     </>
   );
 }
