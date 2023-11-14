593a592f74c74a21dff653f0f5d80f0efe0f0a3a
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-11-14 15:03:20 -0500

    Added shadcn-ui package

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from './components/ui/button';
+
 function App() {
   return (
     <>
       <h1 className="text-4xl text-blue-700 font-bold underline">Hello world!</h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
     </>
   );
 }
