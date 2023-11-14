5fa0cb716523016395b987fbd762c5e9761fe9c8
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-11-14 17:28:21 -0500

    Added small edit to navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,9 @@ const Navbar = () => {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex justify-between h-14 items-center">
-        <h1 className="text-3xl text-blue-700 font-bold">QuantumQuill</h1>
+        <a href="/">
+          <h1 className="text-3xl text-blue-700 font-bold">QuantumQuill</h1>
+        </a>
 
         <div className="flex items-center gap-2">
           <ModeToggle />
