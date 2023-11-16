992e09428607a82cdfc3785725b2d36b5b14af22
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-11-16 12:09:05 -0500

    Fixed body styling and setup main layout

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,12 @@ function App() {
         defaultTheme="system"
         storageKey="vite-ui-theme"
       >
+        {/* Navbar */}
         <Navbar />
 
         <main className="h-full py-6 flex-1 border-b bg-slate-100 dark:bg-black">
-          <div className="container">
+          <div className="container flex flex-col gap-6">
+            {/* Site Directions */}
             <Card className="dark:bg-zinc-900">
               <CardHeader>
                 <CardTitle>Welcome to BlinkReader</CardTitle>
@@ -24,18 +26,28 @@ function App() {
               <CardContent>
                 <p>Card Content</p>
               </CardContent>
-              <CardFooter className='flex flex-col items-start gap-4'>
+              <CardFooter className="flex flex-col items-start gap-4">
                 <p>Card Footer</p>
 
-                <div className='ml-auto flex gap-4'>
-                  <Button variant='outline'>Click me</Button>
-                  <Button variant='default'>Click me</Button>
+                <div className="ml-auto flex gap-4">
+                  <Button variant="outline">Click me</Button>
+                  <Button variant="default">Click me</Button>
                 </div>
               </CardFooter>
             </Card>
+
+            <div className="flex flex-col gap-2">
+              {/* Text Content */}
+              <Card className="w-full max-w-3xl h-96 mx-auto p-6 flex flex-col justify-center items-center dark:bg-zinc-900 text-center">
+                <p className="text-5xl font-medium">Hello world!</p>
+              </Card>
+              {/* Text Menu */}
+              <Card className="w-full max-w-3xl h-12 mx-auto dark:bg-zinc-900"></Card>
+            </div>
           </div>
         </main>
 
+        {/* Footer */}
         <Footer />
       </ThemeProvider>
     </>
