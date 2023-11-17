fb4cb81ef637aed81b995113549d9db9dc342d52
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-11-17 11:47:55 -0500

    Refactored form into dialog component and added hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from '@/components/theme-provider';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
-import TextForm from '@/components/text-form';
+import TextareaFormDialog from '@/components/text-form-dialog';
 
 function App() {
   return (
@@ -15,11 +15,19 @@ function App() {
 
         <main className="h-full py-6 flex-1 border-b bg-slate-100 dark:bg-black">
           <div className="container flex flex-col gap-6">
-
-            <div className="flex flex-col gap-2">
-              <TextForm />
-
-            </div>
+            {/* Hero */}
+            <section className="flex flex-col justify-center items-center space-y-6 md:space-y-12">
+              <h2 className="text-2xl sm:text-3xl lg:text-4xl font-bold flex flex-col text-center">
+                Welcome to <span className="text-4xl sm:text-5xl lg:text-6xl lg:mt-2">BlinkReader!</span>
+              </h2>
+              <p className="max-w-4xl md:text-lg text-center text-muted-foreground">
+                Meet BlinkReader, your literary superhero! With lightning-fast speed and unmatched focus, BlinkReader
+                empowers users to conquer mountains of text in the blink of an eye. Say farewell to sluggish
+                reading—embrace the swift journey through words with your trusty efficiency sidekick!
+              </p>
+              {/* Form Dialog */}
+              <TextareaFormDialog />
+            </section>
           </div>
         </main>
 
---
+++ src/components/text-form-dialog.tsx
@@ -0,0 +1,105 @@
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import {
+  Dialog,
+  DialogContent,
+  DialogClose,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+
+import { Button } from '@/components/ui/button';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import { Textarea } from '@/components/ui/textarea';
+
+const FormSchema = z.object({
+  text: z.string({ required_error: 'Text is required' }).trim(),
+});
+
+const TextareaFormDialog = () => {
+  const [open, setOpen] = useState(false);
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    if (data.text) {
+      // const text = JSON.stringify(data, null, 2);
+      console.log(data);
+      setOpen(false);
+    }
+  }
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <DialogTrigger asChild>
+        <Button
+          variant="default"
+          size="lg"
+          className="w-full sm:w-fit"
+        >
+          Get Started
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="dark:bg-zinc-900 max-w-6xl">
+        <DialogHeader>
+          <DialogTitle>Add Text</DialogTitle>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-full flex flex-col space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="text"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Paste the text you'd like to speed read here"
+                      rows={10}
+                      {...field}
+                      className="h-96"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter className="flex flex-row gap-6">
+              <DialogClose
+                asChild
+                className="w-full"
+              >
+                <Button
+                  type="button"
+                  variant="secondary"
+                >
+                  Close
+                </Button>
+              </DialogClose>
+              <Button
+                type="submit"
+                className="w-full"
+              >
+                Submit
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default TextareaFormDialog;
