d703383ab854304dd07452424952b1de8ad32b7c
KT 2022 <103476893+kt946@users.noreply.github.com>
2023-11-19 16:38:06 -0500

    Added slider component to handle playback

--- src/components/text-player.tsx
+++ src/components/text-player.tsx
@@ -3,6 +3,7 @@ import { FaPlay, FaForward, FaBackward, FaPause } from 'react-icons/fa';
 
 import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
+import { Slider } from '@/components/ui/slider';
 
 import TextHandler from '@/lib/textHandler';
 
@@ -15,20 +16,20 @@ const TextPlayer = ({ readingText }: TextPlayerProps) => {
   const wordGroups = textHandler.wordGroups;
   const wordGroupLength = wordGroups.length;
 
-  const [playingText, setPlayingText] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
   const [index, setIndex] = useState(0);
   const [currentText, setCurrentText] = useState(wordGroups[index]);
 
   // Play text
   const play = () => {
     console.log('Playing through text');
-    setPlayingText(true);
+    setIsPlaying(true);
   };
 
   // Pause text
   const pause = () => {
     console.log('Playing paused');
-    setPlayingText(false);
+    setIsPlaying(false);
   };
 
   // Set index to next word group
@@ -37,12 +38,13 @@ const TextPlayer = ({ readingText }: TextPlayerProps) => {
       setIndex((prevIndex) => prevIndex + 1);
     } else {
       console.log('End of text');
-      setPlayingText(false);
+      setIsPlaying(false);
     }
   };
 
   // Set index to 0; reset text to beginning
   const back = () => {
+    console.log('Back to beginning');
     setIndex(0);
   };
 
@@ -56,15 +58,15 @@ const TextPlayer = ({ readingText }: TextPlayerProps) => {
     let intervalId: NodeJS.Timeout | null = null;
 
     const playHandler = () => {
-      if (index < wordGroupLength - 1 && playingText) {
+      if (index < wordGroupLength - 1 && isPlaying) {
         setIndex((prevIndex) => prevIndex + 1);
       } else {
-        setPlayingText(false);
+        setIsPlaying(false);
       }
     };
 
-    if (playingText) {
-      intervalId = setInterval(playHandler, 500); // Adjust the interval time as needed (in milliseconds)
+    if (isPlaying) {
+      intervalId = setInterval(playHandler, 300); // Adjust the interval time as needed (in milliseconds)
     }
 
     return () => {
@@ -72,7 +74,7 @@ const TextPlayer = ({ readingText }: TextPlayerProps) => {
         clearInterval(intervalId);
       }
     };
-  }, [index, playingText, wordGroupLength]);
+  }, [index, isPlaying, wordGroupLength]);
 
   return (
     <Card className="h-96 sm:min-h-[calc(100vh-203px)] flex flex-col justify-between dark:bg-zinc-900">
@@ -83,44 +85,57 @@ const TextPlayer = ({ readingText }: TextPlayerProps) => {
       <CardContent className="p-0 my-12">
         <p className="text-center text-2xl sm:text-3xl md:text-4xl lg-text-5xl font-semibold">{currentText}</p>
       </CardContent>
-      {/* Player UI */}
-      <CardFooter className="flex justify-center items-center gap-2">
-        <Button
-          type="button"
-          variant="outline"
-          size="lg"
-          onClick={() => back()}
-        >
-          <FaBackward className="h-4 w-4" />
-        </Button>
-        {playingText ? (
-          <Button
-            type="button"
-            variant="outline"
-            size="lg"
-            onClick={() => pause()}
-          >
-            <FaPause className="h-4 w-4" />
-          </Button>
-        ) : (
-          <Button
-            type="button"
-            variant="outline"
-            size="lg"
-            onClick={() => play()}
-          >
-            <FaPlay className="h-4 w-4" />
-          </Button>
-        )}
+      {/* Player Controls */}
+      <CardFooter className="flex flex-col justify-center items-center gap-8 sm:gap-10">
+        {/* Player Slider */}
+        <Slider
+          defaultValue={[0]}
+          max={wordGroupLength - 1}
+          step={1}
+          value={[index]}
+          onValueChange={(value) => setIndex(value[0])}
+          className="cursor-pointer"
+        />
 
-        <Button
-          type="button"
-          variant="outline"
-          size="lg"
-          onClick={() => next()}
-        >
-          <FaForward className="h-4 w-4" />
-        </Button>
+        {/* Player Button Group */}
+        <div className="flex justify-center items-center gap-6">
+          <Button
+            type="button"
+            variant="ghost"
+            size="lg"
+            onClick={() => back()}
+          >
+            <FaBackward className="h-5 w-5" />
+          </Button>
+          {isPlaying ? (
+            <Button
+              type="button"
+              variant="ghost"
+              size="lg"
+              onClick={() => pause()}
+            >
+              <FaPause className="h-5 w-5" />
+            </Button>
+          ) : (
+            <Button
+              type="button"
+              variant="ghost"
+              size="lg"
+              onClick={() => play()}
+            >
+              <FaPlay className="h-5 w-5" />
+            </Button>
+          )}
+
+          <Button
+            type="button"
+            variant="ghost"
+            size="lg"
+            onClick={() => next()}
+          >
+            <FaForward className="h-5 w-5" />
+          </Button>
+        </div>
       </CardFooter>
     </Card>
   );
--- src/lib/textHandler.ts
+++ src/lib/textHandler.ts
@@ -1,16 +1,16 @@
-/** 
+/**
  * TextHandler class to store and process the text input.
  * @class
-*/
+ */
 class TextHandler {
-  textString: string;
-  textArray: string[];
+  originalText: string;
+  splitText: string[];
   wordGroups: string[];
 
   constructor(textString: string, chunkSize: number = 1) {
-    this.textString = textString;
-    this.textArray = textString.split(' ');
-    this.wordGroups = this.groupWordsByChunk(this.textArray, chunkSize);
+    this.originalText = textString;
+    this.splitText = this.originalText.split(' ');
+    this.wordGroups = this.groupWordsByChunk(this.splitText, chunkSize);
   }
 
   /**
@@ -21,17 +21,17 @@ class TextHandler {
    * @returns
    */
   private groupWordsByChunk(textArray: string[], chunkSize: number): string[] {
-    const result = [];
+    const wordGroups = [];
 
     // For loop to iterate over the array of words and group them into chunks separated by spaces.
     // If the chunk size is 1, then the array of words is returned as is.
     // If there are not enough words to fill a chunk, then the remaining words are grouped together.
     for (let i = 0; i < textArray.length; i += chunkSize) {
       const chunk = textArray.slice(i, i + chunkSize).join(' ');
-      result.push(chunk);
+      wordGroups.push(chunk);
     }
 
-    return result;
+    return wordGroups;
   }
 }
 
